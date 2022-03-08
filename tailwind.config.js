8287b922bdaa461c274ada56ca77d463bb05f1be
Oludamilola Osiname <osinameoludamilola@gmail.com>
2022-03-08 17:12:33 +0100

    set up tailwind and other configurations

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    colors: {
+      "vark-blue": "#1d2325",
+      "dark-grayish-blue": "#68707D",
+      "grayish-blue": "#B6BCC8",
+      "light-grayish-blue": "#F7F8FD",
+      orange: "#FF7D1A",
+      "pale-orange": "#FFEDE0",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+
+    fontFamily: {
+      sans: ["Kumbh Sans", "sans-serif"],
+    },
+  },
+};
